0d19b62039aec541759fc194bf71e9e59e5d5c4d
redstoneguy10ls <redstoneguy10ls@gmail.com>
2025-08-27 16:18:00 -0600

    removed the need for a vertical branch

--- src/main/java/com/redstoneguy10ls/lithicaddon/common/items/FruitBasket.java
+++ src/main/java/com/redstoneguy10ls/lithicaddon/common/items/FruitBasket.java
@@ -32,6 +32,7 @@ import static net.dries007.tfc.common.blocks.plant.fruit.Lifecycle.FRUITING;
 public class FruitBasket extends Item {
 
     private boolean gay;
+    private final int range = Helpers.getValueOrDefault(LithicConfig.SERVER.fruitBasketSearchRadius);
     public FruitBasket(Properties pProperties) {
         super(pProperties);
         gay = false;
@@ -86,45 +87,43 @@ public class FruitBasket extends Item {
         //if it's a fruit tree branch
         if( (Helpers.isBlock(state, TFCTags.Blocks.FRUIT_TREE_BRANCH) ) && state.canSurvive(level,pos) && tree > -1)
         {
-            if( (!Helpers.isBlock(level.getBlockState(pos.below()),TFCTags.Blocks.FRUIT_TREE_LEAVES) || !Helpers.isBlock(level.getBlockState(pos.below()),TFCTags.Blocks.FRUIT_TREE_BRANCH) )
-                    && state.getValue(PipeBlock.UP))
+            //if( (!Helpers.isBlock(level.getBlockState(pos.below()),TFCTags.Blocks.FRUIT_TREE_LEAVES) || !Helpers.isBlock(level.getBlockState(pos.below()),TFCTags.Blocks.FRUIT_TREE_BRANCH) )
+            //        && state.getValue(PipeBlock.UP))
+            int fruitingLeaves = 0;
+            final BlockPos min = pos.offset(range*-1, 0, range*-1);
+            final BlockPos max = pos.offset(1, 7, 1);
+            if(level.hasChunksAt(min, max))
             {
-                int fruitingLeaves = 0;
-                final BlockPos min = pos.offset(-1, 0, -1);
-                final BlockPos max = pos.offset(1, 7, 1);
-                if(level.hasChunksAt(min, max))
+                for (BlockPos pos1 : BlockPos.betweenClosed(min, max))
                 {
-                    for (BlockPos pos1 : BlockPos.betweenClosed(min, max))
-                    {
-                        final BlockState state1 = level.getBlockState(pos1);
+                    final BlockState state1 = level.getBlockState(pos1);
 
-                        if (Helpers.isBlock(state1, FruitPicker.values()[tree].getLeaf() ))
+                    if (Helpers.isBlock(state1, FruitPicker.values()[tree].getLeaf() ))
+                    {
+                        if (state1.getValue(LIFECYCLE) == FRUITING)
                         {
-                            if (state1.getValue(LIFECYCLE) == FRUITING)
-                            {
-                                fruitingLeaves++;
-                                level.setBlockAndUpdate(pos1, state1.setValue(LIFECYCLE, Lifecycle.HEALTHY));
-                            }
+                            fruitingLeaves++;
+                            level.setBlockAndUpdate(pos1, state1.setValue(LIFECYCLE, Lifecycle.HEALTHY));
                         }
                     }
-                    
-                    if (isRegularBasket() && fruitingLeaves > 0)
-                    {
-                        if (level.random.nextDouble() <= Helpers.getValueOrDefault(LithicConfig.SERVER.fruitTreeBranchBreakChance))
-                        {
-                            level.destroyBlock(pos, true);
-                        }
-                    }
-                    held.hurtAndBreak(fruitingLeaves, player, p -> {});
-                    while (fruitingLeaves > 0)
-                    {
-                        player.getInventory().add(new ItemStack(FruitPicker.values()[tree].getFruit(), 1));
-                        player.playSound(SoundEvents.ITEM_PICKUP,0.1f,1);
-                        fruitingLeaves--;
-                    }
-
-
                 }
+                
+                if (isRegularBasket() && fruitingLeaves > 0)
+                {
+                    if (level.random.nextDouble() <= Helpers.getValueOrDefault(LithicConfig.SERVER.fruitTreeBranchBreakChance))
+                    {
+                        level.destroyBlock(pos, true);
+                    }
+                }
+                held.hurtAndBreak(fruitingLeaves, player, p -> {});
+                while (fruitingLeaves > 0)
+                {
+                    player.getInventory().add(new ItemStack(FruitPicker.values()[tree].getFruit(), 1));
+                    player.playSound(SoundEvents.ITEM_PICKUP,0.1f,1);
+                    fruitingLeaves--;
+                }
+
+
             }
             return InteractionResult.SUCCESS;
         }
