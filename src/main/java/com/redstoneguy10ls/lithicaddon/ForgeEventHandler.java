e66e70eb6ccec790aaa4db601640c852825b7f2d
redstoneguy10ls <redstoneguy10ls@gmail.com>
2025-08-27 13:49:14 -0600

    changed some logic, so it's only checking every 10 ticks to see if players have moths in their inventory. should cause less lag

--- src/main/java/com/redstoneguy10ls/lithicaddon/ForgeEventHandler.java
+++ src/main/java/com/redstoneguy10ls/lithicaddon/ForgeEventHandler.java
@@ -1,33 +1,24 @@
 package com.redstoneguy10ls.lithicaddon;
 
-import com.eerussianguy.firmalife.common.blockentities.VatBlockEntity;
-import com.eerussianguy.firmalife.common.blocks.VatBlock;
 import com.redstoneguy10ls.lithicaddon.common.blocks.LithicBlocks;
 import com.redstoneguy10ls.lithicaddon.common.capabilities.moth.MothCapability;
 import com.redstoneguy10ls.lithicaddon.common.items.LithicItems;
 import com.redstoneguy10ls.lithicaddon.util.advancements.LithicAdvancements;
-import net.dries007.tfc.common.TFCTags;
 import net.dries007.tfc.common.capabilities.forge.ForgingBonus;
-import net.dries007.tfc.common.items.TFCItems;
 import net.dries007.tfc.util.Helpers;
 import net.dries007.tfc.util.Metal;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.player.PlayerDestroyItemEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.items.ItemHandlerHelper;
 
 public class ForgeEventHandler {
 
@@ -44,15 +35,18 @@ public class ForgeEventHandler {
     {
         if(event.getEntity() instanceof ServerPlayer)
         {
-            Inventory inv = event.getEntity().getInventory();
-            for(ItemStack stack : inv.items)
+            if (event.getEntity().level().getGameTime() % 10 == 0)
             {
-                stack.getCapability(MothCapability.CAPABILITY).ifPresent(moth->{
-                    if(moth.isMoth())
-                    {
-                        moth.kill();
-                    }
-                });
+                Inventory inv = event.getEntity().getInventory();
+                for(ItemStack stack : inv.items)
+                {
+                    stack.getCapability(MothCapability.CAPABILITY).ifPresent(moth->{
+                        if(moth.isMoth())
+                        {
+                            moth.kill();
+                        }
+                    });
+                }
             }
         }
         
